8e057024941c5e7ff34632da5d9d29dfb8665062
Gianluca Andretta <Gianluca.Andretta@gmail.com>
2022-01-15 12:56:54 +0100

    quick messy fix for Search and activeTags

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,7 +2,6 @@ import React, { useCallback, useEffect, useMemo, useState } from "react";
 import Link from 'next/link';
 import { Client } from "@notionhq/client";
 
-import { useFetch } from "../components/useFetch";
 import SearchBar from "../components/SearchBar";
 
 // import { databaseId, getPosts } from "../lib/notion";          
@@ -11,19 +10,16 @@ import * as utils from "../lib/utils";
 
 
 
-
-
 export async function getStaticProps() {
   let tags = utils.formatTags(await notion.getDatabase(notion.databaseId));
   const posts = utils.formatPostsToIds(await notion.getPosts(notion.databaseId));
-  // var categories = await tags.map(async (tag) =>({ [tag.name] : (await notion.getPosts(notion.databaseId,[tag.name]))}));
 
-  for (let i = 0; i < tags.length; i++) {
-    tags[i].posts = await notion.getPosts(notion.databaseId, [tags[i].name])
+  for (let tag of tags) {
+    tag.posts = await notion.getPosts(notion.databaseId, [tag.name]);
   }
-  // tags.map( (tag,id) => console.log(tag.name) );
-  let postsFromTags = utils.formatPostsToIds(tags.reduce((p, c) => [...p, ...c.posts], []))
-  console.log(postsFromTags);
+
+  // example async await with map method using promise.all to resolve the array of promises
+  // let ciccio = await Promise.all(tags2.map(async (tag) => {tag.posts = await notion.getPosts(notion.databaseId, [tag.name]);return tag} ));
 
   return ({
     props: { tags, posts },
@@ -34,66 +30,62 @@ export async function getStaticProps() {
 
 export default function Blog({ tags, posts }) {
   const [postTitles, setPostTitles] = useState(posts);
+  const [activePosts, setActivePosts] = useState(posts);
   const [postTags, setPostTags] = useState(tags);
   const [activeTags, setActiveTags] = useState([]);
   const [inputText, setInputText] = useState("");
-  // const { data } 
 
 
 
-  function handleSearch(e) {
-    setInputText(e.target.value.toLowerCase());
-    setPostTitles(
-      posts.filter(p => p.name.toLowerCase().includes(inputText))
-    )
+  function handleSearch() {
+
   }
 
 
-  
-
-
-  async function handleTags(tag) {
-    let e = activeTags.indexOf(tag)
-    if (e == -1) {
-      setActiveTags([...activeTags, tag])
+  async function handleTags(tagName) {
+    if (activeTags.indexOf(tagName) == -1) {
+      setActiveTags([...activeTags, tagName])
     } else {
-      setActiveTags(activeTags.filter(t => (t != tag)))
+      setActiveTags(activeTags.filter(t => (t != tagName)))
     }
   }
+
+  useEffect(() => {
+    let temp;
+    if (activeTags.length == 0) {
+      setPostTitles(posts);
+      setActivePosts(posts)
+    }
+    else {
+      temp = utils.formatPostsToIds(utils.mergedPostsByTags(tags, activeTags));
+      setPostTitles(temp);
+      setActivePosts(temp)
+    }
+  }, [activeTags, posts, tags])
 
 
   useEffect(() => {
-    // utils.getFilteredPosts(activeTags).then(res => setPostTitles(res));
-    if (activeTags.length == 0) { 
-      setPostTitles(posts);
-    }
-    else {
-      setPostTitles(
-        utils.formatPostsToIds(
-          tags.filter(tag => activeTags.includes(tag.name)).reduce((a, p) => ([...a, ...p.posts]), [])
-        )
-      )
-    }
 
-  }, [activeTags])
+    setPostTitles(activePosts.filter(p => p.name.toLowerCase().includes(inputText.toLowerCase())));
 
+  }, [inputText, activePosts])
 
 
   useEffect(() => {
     console.log(posts);
     console.log(tags);
-  }, [posts, tags ]);
+  }, [posts, tags]);
 
   return (
     <div className="Blog maxWidth42 mxAuto">
       <div className="content">
         <h1>Blog</h1>
 
-        <SearchBar handleChange={handleSearch}></SearchBar>
+        <SearchBar handleChange={(e) => setInputText(e.target.value)}></SearchBar>
 
         {(postTags) ?
           <div className="tags">
-            <button className={activeTags.length == 0 ? "tag hidden" : "tag"} onClick={() => { setActiveTags([]); setPostTitles(posts) }}>&#10005;</button>
+            <button className={activeTags.length == 0 ? "tag hidden" : "tag"} onClick={() => { setActiveTags([]);  setActivePosts(posts); }}>&#10005;</button>
             {postTags.map((tag) => (
               <button key={tag.id} className={activeTags.indexOf(tag.name) == -1 ? "tag" : "tag tagActive"} type="button" onClick={() => handleTags(tag.name)}>{tag.name}</button>
             ))}
@@ -103,8 +95,8 @@ export default function Blog({ tags, posts }) {
         {(postTitles) ?
           <ul>{postTitles.map((titolo, index) => (
             <li key={index}>
-              <Link key={titolo.id} href={`/blog/${postTitles[titolo.id]}`} passHref>
-                <a onClick={() => console.log(`apro la pagina ${titolo.name}`)}>{titolo.name}</a>
+              <Link key={titolo.id} href={`/blog/${titolo.id}`} passHref>
+                <a onClick={() => console.log(`apro la pagina ${titolo.name} con id ${titolo.id}`)}>{titolo.name}</a>
               </Link>
             </li>
           ))}</ul>
