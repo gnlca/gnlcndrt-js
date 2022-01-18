d95a5dc6ace28b5d53a8146fbaa29048972c8b77
Gianluca Andretta <Gianluca.Andretta@gmail.com>
2022-01-18 21:48:45 +0100

    fix vercel problem
    git isn't casesensitive so vercel can't find the renamed file

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 export default function Layout({ children }) {
     return (
         <>
-            <Navbar />
+            <Navbar/>
             <main>{children}</main>
             {/* eventualmente un footer */}
         </>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/layout.css'
-import Layout from '../components/Layout';
+
+import Layout from "../components/Layout";
 
 
 function MyApp({ Component, pageProps }) {
